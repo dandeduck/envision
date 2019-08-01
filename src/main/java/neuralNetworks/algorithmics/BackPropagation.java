6da3673cafae51630eec5b5f40af6c6e7b214aa7
DanielTheCUPMaker <danielmkhlv@gmail.com>
2019-08-01 17:20:01 +0300

    retirned hasLearned

--- src/main/java/neuralNetworks/algorithmics/BackPropagation.java
+++ src/main/java/neuralNetworks/algorithmics/BackPropagation.java
@@ -1,6 +1,7 @@
 package neuralNetworks.algorithmics;
 
 import dataTypes.Data;
+import neuralNetworks.objects.basicObjects.Neuron;
 import neuralNetworks.objects.complexObjects.Layer;
 import neuralNetworks.objects.complexObjects.WeightsMat;
 
@@ -23,6 +24,16 @@ public class BackPropagation implements TrainingAlgorithm {
 
     @Override
     public boolean hasLearned(Layer outputLayer, Data outputPattern) {
-        return false;
+        return outputLayer.stream()
+                .map(Neuron::new)
+                .allMatch(n -> constrained(outputLayer.get(outputLayer.size()-1).get(),
+                        outputPattern.getOutputPoint(outputLayer.indexOf(n)) - acceptedError,
+                        outputPattern.getOutputPoint(outputLayer.indexOf(n)) + acceptedError));
+    }
+
+    private boolean constrained(double val, double min, double max) {
+        min = min < 0 ? 0 : min;
+        max = max > 1 ? 1 : max;
+        return val >= min && val <= max;
     }
 }
